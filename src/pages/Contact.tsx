1348e62049d1e16cdc4469c1ccc7fae2559928a7
sumaitasiddiky <sumaitasiddiky@gmail.com>
2023-03-14 23:18:04 +1100

    improved contact text

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,15 +3,15 @@ import React from "react";
 export function Contact() {
 
     const email = "adventurebitesize@gmail.com"
+    
     return (
         <div className="contact">
             <br></br>
-            <h3>Contact form coming soon</h3>
             <br></br>
             <p>We are dedicated to providing you with a comprehensive travel planning experience, 
                 serving as your one-stop-shop for all your travel needs. 
-                If you require more personalized and detailed travel itineraries, please don't hesitate to contact us at 
-                {email}  
+                If you require more personalized and detailed travel itineraries, 
+                please don't hesitate to contact us at <i>{email}</i>
             </p>
 
         </div>

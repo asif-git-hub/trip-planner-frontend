3da9ca1cee76bf9ae1e17a821f702be1e20f7d4a
Asif Ul Alam <asifalam0308@gmail.com>
2023-03-20 22:45:04 +1100

    remove comment

--- src/components/DestinationInput.tsx
+++ src/components/DestinationInput.tsx
@@ -26,7 +26,6 @@ export function DestinationInput({data, setData}: DestinationFormPropType) {
   
     const scriptStatus = useScript(
       // By default, Google Places will attempt to guess your language based on your country.
-      // However, you may specify it explicitly with the `language` parameter.
       `https://maps.googleapis.com/maps/api/js?language=en&key=${GOOGLE_API_KEY}&libraries=places&callback=Function.prototype`
     )
 

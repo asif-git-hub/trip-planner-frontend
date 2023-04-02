89b8dd2b2a8be696575127697784fa490606f2e8
Asif Ul Alam <asifalam0308@gmail.com>
2023-04-03 00:28:37 +1000

    add preset trips

---
+++ src/components/PresetTrips.tsx
@@ -0,0 +1,38 @@
+import React, { Dispatch, SetStateAction } from "react"
+import { presetTrips } from "../data/preset.trips"
+import { Trip } from "./Trip"
+import { ItineraryRequestType } from "../types/request.types"
+
+type PresetTripsPropType = {
+  setData: Dispatch<SetStateAction<ItineraryRequestType>>
+  setLoading: Dispatch<SetStateAction<boolean>>
+  setErrored: Dispatch<SetStateAction<boolean>>
+  setItinerary: Dispatch<SetStateAction<string>>
+  setCafeRecommendations: Dispatch<SetStateAction<string>>
+  setRestaurantRecommendations: Dispatch<SetStateAction<string>>
+}
+
+export function PresetTrips({ setData, setLoading, setErrored, setItinerary, setCafeRecommendations, setRestaurantRecommendations }: PresetTripsPropType) {
+  return (
+    <div className="preset-trips-container">
+      <div className="preset-trips-heading">
+        <h4>Not sure where to go? Try these suggestions</h4>
+      </div>
+      <div className="underline"></div>
+      <div className="trips-container">
+        {presetTrips.map((trip, id) => {
+            return <Trip key={id} 
+            {...trip} 
+            setData={setData} 
+            setLoading={setLoading} 
+            setErrored={setErrored}
+            setItinerary={setItinerary}
+            setCafeRecommendations={setCafeRecommendations}
+            setRestaurantRecommendations={setRestaurantRecommendations}
+            ></Trip>
+        })}
+      </div>
+
+    </div>
+  )
+}

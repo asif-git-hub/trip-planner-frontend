9483b0e94eca459d504c22342c80c6f01d5c019f
Asif Ul Alam <asifalam0308@gmail.com>
2023-04-08 00:59:57 +1000

    add public path

--- webpack.frontend.config.js
+++ webpack.frontend.config.js
@@ -15,6 +15,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
+    publicPath: '/',
   },
   resolve: {
     extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],

d17003d682a564c59d8e4bcb940786a2f4e1cbf5
Asif Ul Alam <asifalam0308@gmail.com>
2023-05-14 18:17:17 +1000

    format

--- src/components/EditActivity.tsx
+++ src/components/EditActivity.tsx
@@ -4,15 +4,15 @@ import { ActivityType } from "../types/response.types"
 import { AiOutlineDown, AiOutlineUp } from "react-icons/ai"
 
 type EditActivityPropType = {
-    dailyActivities: ActivityType[]
-    setDailyActivities: Dispatch<React.SetStateAction<ActivityType[]>>
-    currentOrder: number
+  dailyActivities: ActivityType[]
+  setDailyActivities: Dispatch<React.SetStateAction<ActivityType[]>>
+  currentOrder: number
 }
 
 export function EditActivity({
-    dailyActivities,
-    setDailyActivities,
-    currentOrder,
+  dailyActivities,
+  setDailyActivities,
+  currentOrder,
 }: EditActivityPropType) {
   const [expand, setExpand] = useState(false)
 
@@ -30,7 +30,6 @@ export function EditActivity({
       setDailyActivities(newDailyActivities)
     }
     setExpand(!expand)
-
   }
 
   function moveDown() {
@@ -57,21 +56,30 @@ export function EditActivity({
           setExpand(!expand)
         }}
       >
-        <FiEdit className="edit-icon"></FiEdit>  Edit  {expand? <AiOutlineUp className="edit-toggle-icon"/> : <AiOutlineDown className="edit-toggle-icon"></AiOutlineDown>}
+        <FiEdit className="edit-icon"></FiEdit> Edit{" "}
+        {expand ? (
+          <AiOutlineUp className="edit-toggle-icon" />
+        ) : (
+          <AiOutlineDown className="edit-toggle-icon"></AiOutlineDown>
+        )}
       </button>
 
       {expand ? (
         <div className="expandable-btn-container">
           <button className="" onClick={moveUp}>
             <div className="expandable-btn-content">
-                <div className="expandable-btn-icon"><AiOutlineUp></AiOutlineUp></div>
-                <div className="expandable-btn-text">Move Up</div>
+              <div className="expandable-btn-icon">
+                <AiOutlineUp></AiOutlineUp>
+              </div>
+              <div className="expandable-btn-text">Move Up</div>
             </div>
           </button>
           <button className="" onClick={moveDown}>
             <div className="expandable-btn-content">
-                <div className="expandable-btn-icon"><AiOutlineDown></AiOutlineDown></div>
-                <div className="expandable-btn-text">Move Down</div>
+              <div className="expandable-btn-icon">
+                <AiOutlineDown></AiOutlineDown>
+              </div>
+              <div className="expandable-btn-text">Move Down</div>
             </div>
           </button>
         </div>

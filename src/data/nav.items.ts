02c39da1099d1cb767cf162023ca0cb304145980
Asif Ul Alam <asifalam0308@gmail.com>
2023-05-18 20:35:26 +1000

    sign up

--- src/data/nav.items.ts
+++ src/data/nav.items.ts
@@ -4,24 +4,25 @@ const callbackUrl = getEnvVar("REACT_APP_CALLBACK_URL")
 const env = getEnvVar("REACT_APP_ENVIRONMENT")
 const cognitoClientId = getEnvVar("REACT_APP_COGNITO_CLIENT_ID")
 
-const url = `https://${env.toLowerCase()}-enchantrek-domain.auth.ap-southeast-2.amazoncognito.com/login?client_id=${cognitoClientId}&response_type=code&scope=email+openid&redirect_uri=${callbackUrl}`
+const loginUrl = `https://${env.toLowerCase()}-enchantrek-domain.auth.ap-southeast-2.amazoncognito.com/login?client_id=${cognitoClientId}&response_type=code&scope=email+openid&redirect_uri=${callbackUrl}`
+const signupUrl = `https://${env.toLowerCase()}-enchantrek-domain.auth.ap-southeast-2.amazoncognito.com/signup?client_id=${cognitoClientId}&response_type=code&scope=email+openid&redirect_uri=${callbackUrl}`
 
 export const links = [
   {
     id: 1,
-    url: url,
+    url: loginUrl,
     text: 'login',
   },
   {
     id: 2,
+    url: signupUrl,
+    text: 'sign up',
+  },
+  {
+    id: 3,
     url: "/about",
     text: "about",
   },
-  // {
-  //   id: 3,
-  //   url: '/blogs',
-  //   text: 'blogs',
-  // },
   {
     id: 4,
     url: "/contact",
